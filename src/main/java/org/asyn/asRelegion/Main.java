22b5ba3a79c0dfb2af978e3c3acdf1931bdc3197
Himyuc <kasumikoarglol@gmail.com>
2026-07-28 00:21:31 -0300

    API: MMoCore-MythicLib Integration.

--- src/main/java/org/asyn/asRelegion/Main.java
+++ src/main/java/org/asyn/asRelegion/Main.java
@@ -1,13 +1,19 @@
 package org.asyn.asRelegion;
 
+import io.lumine.mythic.lib.api.player.MMOPlayerData;
+import io.lumine.mythic.lib.api.stat.StatInstance;
+import io.lumine.mythic.lib.api.stat.StatMap;
+import net.Indyuce.mmocore.api.player.PlayerData;
 import org.asyn.asRelegion.commands.AdminMain;
 import org.asyn.asRelegion.commands.PlayerMain;
-import org.asyn.asRelegion.commands.ReligionMain;
 import org.asyn.asRelegion.listeners.PlayerJoin;
 import org.asyn.asRelegion.utils.Messages;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.UUID;
 
 import static org.asyn.asRelegion.utils.Messages.asciiart;
 
@@ -15,6 +21,18 @@ public final class Main extends JavaPlugin {
 
     public static String prefix = "&bAsReligion &7>>";
     public static String ingameprefix = "&e[Religion] &7>> ";
+
+    Player player = Bukkit.getPlayer(Bukkit.getName());
+
+    // MMOCore (https://docs.phoenixdevt.fr/mmocore/api/api)
+    PlayerData datammo = PlayerData.get(player);
+    int plevel = datammo.getLevel();
+
+    // MythicLib (https://docs.phoenixdevt.fr/mythiclib/api/stats)
+
+    MMOPlayerData mythdata = MMOPlayerData.get(player);
+    StatMap statMap = mythdata.getStatMap();
+    StatInstance magicDamageStat = statMap.getInstance("MAGIC_DAMAGE" ); // Dentro del parentesis puedes poner cualquier stat existente.
 
     @Override
     public void onEnable() {
